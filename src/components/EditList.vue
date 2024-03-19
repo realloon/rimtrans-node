7296b75c76c600f11c0c170356792b83374f53fb
realon <sincerajn@outlook.com>
2024-03-19 23:46:54 +0800

    ~

---
+++ src/components/EditList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const props = defineProps<{
+  list: Array<string>
+  add: Function
+  remove: Function
+  // size?: string
+}>()
+
+const input = ref('')
+</script>
+
+<template>
+  <ol>
+    <li v-for="item in props.list">
+      <div class="item">
+        <span>{{ item }}</span>
+        <icon @click="remove(item)" type="close0" />
+      </div>
+    </li>
+  </ol>
+
+  <div class="add-wrapper">
+    <input v-model.trim="input" name="dependency" type="text" />
+    <button @click.prevent="add"><icon type="plus" /></button>
+  </div>
+</template>
+
+<style scoped></style>
