3013f5914efe52802d0e2fc8a794c4361f5b9e0e
realon <sincerajn@outlook.com>
2024-03-21 03:03:52 +0800

    Complete reconstruction of extraction function.

--- src/helper/InputFile/extractXmlsFromZip.ts
+++ src/helper/InputFile/extractXmlsFromZip.ts
@@ -1,43 +1,62 @@
 import JSZip from 'jszip'
 
-const unzip = new JSZip()
-
-const regexs = {
-  defs: /\d.\d\/Defs\/.+.xml$/,
-  about: /About\/About.xml$/,
-}
-
 type Xmls = {
   defXmls: Array<string>
   aboutXml: string
 }
 
-type ZipObj = JSZip.JSZipObject
+const regexs = {
+  defs: /\d+\.\d+\/Defs\/.+.xml$/,
+  about: /About\/About.xml$/,
+}
 
-// TODO: 未处理不合规目录结构
-const extractXmlsFromZip = async (zip: ArrayBuffer): Promise<Xmls> =>
-  new Promise((resolve, reject) => {
-    unzip.loadAsync(zip).then(async targrt => {
-      // defs
-      const defXmls = await Promise.all(
-        Object.values(targrt.files)
-          .filter(({ name }) => regexs.defs.test(name))
-          .map(file => (targrt.file(file.name) as ZipObj).async('string'))
-      )
+const getVersion = (path: string): number => {
+  const match = path.match(/\d+\.\d+/)
+  return match ? parseFloat(match[0]) : 0
+}
 
-      // about
-      const aboutPath = (
-        Object.values(targrt.files).find(({ name }) =>
-          regexs.about.test(name)
-        ) as ZipObj
-      ).name
-      const aboutXml = await (targrt.file(aboutPath) as ZipObj).async('string')
+const isNotMeta = ([name]: [string, JSZip.JSZipObject]) =>
+  !name.startsWith('__MACOSX/')
+const isDefFile = ([name]: [string, JSZip.JSZipObject]) =>
+  regexs.defs.test(name)
+const isAboutFile = ([name]: [string, JSZip.JSZipObject]) =>
+  regexs.about.test(name)
 
-      resolve({
-        defXmls,
-        aboutXml,
-      })
-    })
-  })
+const getLatestDefFiles = (entries: [string, JSZip.JSZipObject][]) => {
+  return entries.filter(isDefFile).reduce(
+    (latestFiles, [name, zipEntry]) => {
+      const version = getVersion(name)
+      if (!latestFiles.version || version > latestFiles.version) {
+        return { version, files: [zipEntry] }
+      }
+      if (version === latestFiles.version) {
+        latestFiles.files.push(zipEntry)
+      }
+      return latestFiles
+    },
+    { version: 0, files: [] as JSZip.JSZipObject[] }
+  ).files
+}
+
+const extractXmlsFromZip = async (zip: ArrayBuffer): Promise<Xmls> => {
+  const target = await new JSZip().loadAsync(zip)
+
+  const entries = Object.entries(target.files).filter(isNotMeta)
+  const latestDefEntries = getLatestDefFiles(entries)
+  const aboutEntry = entries.find(isAboutFile)?.[1]
+
+  if (!aboutEntry) {
+    throw new Error('No valid About.xml file found.')
+  }
+
+  const defXmlPromises = latestDefEntries.map(file => file.async('string'))
+  const aboutXmlPromise = aboutEntry.async('string')
+  const [defXmls, aboutXml] = await Promise.all([
+    Promise.all(defXmlPromises),
+    aboutXmlPromise,
+  ])
+
+  return { defXmls, aboutXml }
+}
 
 export default extractXmlsFromZip
