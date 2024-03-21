882bd2a3925ff4c9a5d93cab68057870292811f7
realon <sincerajn@outlook.com>
2024-03-21 19:19:20 +0800

    Refactoring to first merge the Dom and then extract it.

--- src/helper/InputFile/extractNodesFromXmls.ts
+++ src/helper/InputFile/extractNodesFromXmls.ts
@@ -1,16 +1,33 @@
 import { useSettingStore } from '@/stores/setting'
 
-const extractNodesFromXmls = async (xmls: Array<string>) => {
-  const parser = new DOMParser()
-  const doms = xmls.map(xml => parser.parseFromString(xml, 'application/xml'))
-
+async function extractNodesFromXmls(xmls: Array<string>) {
+  const dom = mergedDom(xmls)
   const { extractFields } = useSettingStore()
-  return doms
-    .map(dom => extractFields.map(rule => dom.querySelectorAll(rule)))
-    .flat()
-    .filter(item => item.length !== 0)
-    .map(item => [...item])
-    .flat()
+
+  return extractFields
+    .map((rule: string) => dom.getElementsByTagName(rule))
+    .filter(elements => elements.length > 0)
+    .flatMap(elements => [...elements])
+}
+
+function mergedDom(xmls: Array<string>): XMLDocument {
+  const parser = new DOMParser()
+
+  const targetDom = document.implementation.createDocument(null, 'Defs', null)
+  const targetDefsEl = targetDom.getElementsByTagName('Defs')[0]
+
+  xmls.forEach(xml => {
+    const dom = parser.parseFromString(xml, 'application/xml')
+    const defsEl = dom.getElementsByTagName('Defs')[0]
+
+    if (!defsEl) return
+
+    Array.from(defsEl.children).forEach(defEl =>
+      targetDefsEl.appendChild(defEl)
+    )
+  })
+
+  return targetDom
 }
 
 export default extractNodesFromXmls
