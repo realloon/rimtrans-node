8cd60b72e091f1fd08751839e9c1da072588b92d
realon <sincerajn@outlook.com>
2024-03-21 17:28:36 +0800

    Remove unnecessary alert prompts.

--- src/helper/InputFile/loadFile.ts
+++ src/helper/InputFile/loadFile.ts
@@ -7,7 +7,7 @@ import extractProject from './extractProject'
 export default async function loadFile(event: Event) {
   const target = event.target as HTMLInputElement
   const file = (target.files as FileList)[0]
-  if (!file) return alert('未检测到上传文件！')
+  if (!file) return
 
   const content = await readFile(file)
   const { defs, about } = await extractProject(content)
