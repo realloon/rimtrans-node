902166c37189b7b494a823d20249bd1d929474be
realon <sincerajn@outlook.com>
2024-03-24 10:51:52 +0800

    Unified code style.

--- src/views/setting/SettingView.vue
+++ src/views/setting/SettingView.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import EditList from '@/components/EditList.vue'
 import { useSettingStore } from '@/stores/setting'
-const settingStore = useSettingStore()
+const setting = useSettingStore()
 </script>
 
 <template>
   <main>
     <h2>Setting (WIP)</h2>
     <form>
-      
-      <edit-list :list="settingStore.extractFields" icon="search1" :header="'提取字段'" orderly/>
+      <edit-list
+        icon="search1"
+        :list="setting.extractFields"
+        :header="'提取字段'"
+        orderly
+      />
     </form>
   </main>
 </template>
