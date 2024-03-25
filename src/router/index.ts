d843f545bb62ff9285be38e0be07b954eae5f971
realon <sincerajn@outlook.com>
2024-03-26 02:15:07 +0800

    Unified sync loading of view components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import homeView from '@/views/home/HomeView.vue'
 import DefsView from '@/views/defs/DefsView.vue'
+import AboutView from '@/views/about/AboutView.vue'
+import ExportView from '@/views/export/ExportView.vue'
+import SettingView from '@/views/setting/SettingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,17 +21,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('@/views/about/AboutView.vue'),
+      component: AboutView,
     },
     {
       path: '/export',
       name: 'export',
-      component: () => import('@/views/export/ExportView.vue'),
+      component: ExportView,
     },
     {
       path: '/setting',
       name: 'setting',
-      component: () => import('@/views/setting/SettingView.vue'),
+      component: SettingView,
     },
   ],
 })
