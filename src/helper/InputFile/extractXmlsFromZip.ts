7d3bff252aca4142cc73946d4367944afcc121f5
realon <sincerajn@outlook.com>
2024-03-28 00:01:18 +0800

    Unified code style.

--- src/helper/InputFile/extractXmlsFromZip.ts
+++ src/helper/InputFile/extractXmlsFromZip.ts
@@ -1,11 +1,5 @@
 import JSZip from 'jszip'
 
-type Xmls = {
-  defXmls: Array<string>
-  aboutXml: string
-  cover: Blob
-}
-
 const regexs = {
   defs: /\d+\.\d+\/Defs\/.+.xml$/,
   about: /About\/About.xml$/,
@@ -24,7 +18,7 @@ const getVersion = (path: string): number => {
   return match ? parseFloat(match[0]) : 0
 }
 
-const getLatestDefFiles = (entries: [string, JSZip.JSZipObject][]) =>
+const getLatestDefFiles = (entries: Array<Name>) =>
   entries.filter(isDefFile).reduce(
     (latestFiles, [name, zipEntry]) => {
       const version = getVersion(name)
@@ -39,29 +33,31 @@ const getLatestDefFiles = (entries: [string, JSZip.JSZipObject][]) =>
     { version: 0, files: [] as JSZip.JSZipObject[] }
   ).files
 
-const extractXmlsFromZip = async (zip: ArrayBuffer): Promise<Xmls> => {
+const extractXmlsFromZip = async (
+  zip: ArrayBuffer
+): Promise<{
+  defXmls: Array<string>
+  aboutXml: string
+  cover: Blob
+}> => {
   const target = await new JSZip().loadAsync(zip)
-
   const entries = Object.entries(target.files).filter(isNotMeta)
 
   // defs
   const latestDefEntries = getLatestDefFiles(entries)
   const defXmlPromises = latestDefEntries.map(file => file.async('string'))
+  const defXmls = await Promise.all(defXmlPromises)
 
   // about
   const aboutEntry = entries.find(isAboutFile)?.[1]
   if (!aboutEntry) throw new Error('No valid About.xml file found.')
   const aboutXmlPromise = aboutEntry.async('string')
+  const aboutXml = await aboutXmlPromise
 
   // cover
   const coverEnter = entries.find(isCoverFile)?.[1]
   if (!coverEnter) throw new Error('No valid preview.png file found.')
   const cover = await coverEnter.async('blob')
-
-  const [defXmls, aboutXml] = await Promise.all([
-    Promise.all(defXmlPromises),
-    aboutXmlPromise,
-  ])
 
   return { defXmls, aboutXml, cover }
 }
